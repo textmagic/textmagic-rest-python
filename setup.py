fd78228a2e5d877f152b0df65d8306cf9d90bcf6
Andrey Larionov <andrey@textmagic.biz>
2015-08-27 17:56:07 +0700

    Package dist version changed.

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ REQUIRES = ['httplib2', 'six']
 
 if sys.version_info < (2, 6):
     REQUIRES.append('simplejson')
-if sys.version_info >= (3, 0):
-    REQUIRES.append('pysocks')
 
 setup(
     name="textmagic",
@@ -19,30 +17,25 @@ setup(
     description="TextMagic APIv2 client",
     author="Textmagic",
     author_email="support@textmagic.com",
-    url="http://code.google.com/p/textmagic-sms-api-python/",
+    url="https://github.com/textmagic/textmagic-rest-python",
     keywords=["textmagic"],
     install_requires=REQUIRES,
-    extras_require={
-        ':python_version=="3.2"': ['pysocks'],
-        ':python_version=="3.3"': ['pysocks'],
-        ':python_version=="3.4"': ['pysocks'],
-    },
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         'Intended Audience :: Information Technology',
         'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Communications :: Telephony",
         ],
